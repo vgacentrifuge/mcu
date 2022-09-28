a4e6e267fd307a04a88a0ba1c23d240cab7b2040
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2022-09-28 16:00:14 +0200

    Add basic debug functionality

--- src/main.c
+++ src/main.c
@@ -16,55 +16,12 @@
  ******************************************************************************/
 #include "sl_component_catalog.h"
 #include "sl_system_init.h"
-#include "app.h"
 #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
 #include "sl_power_manager.h"
 #endif
 #include "sl_system_process_action.h"
-#include "efm32gg990f1024.h"
-
-void setupSWOForPrint(void)
-{
-  /* Enable GPIO clock. */
-  CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
-
-  /* Enable Serial wire output pin */
-  GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
-
-#if defined(_EFM32_GIANT_FAMILY) || defined(_EFM32_LEOPARD_FAMILY) || defined(_EFM32_WONDER_FAMILY)
-  /* Set location 0 */
-  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
-
-  /* Enable output on pin - GPIO Port F, Pin 2 */
-  GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
-  GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
-#else
-  /* Set location 1 */
-  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) |GPIO_ROUTE_SWLOCATION_LOC1;
-  /* Enable output on pin */
-  GPIO->P[2].MODEH &= ~(_GPIO_P_MODEH_MODE15_MASK);
-  GPIO->P[2].MODEH |= GPIO_P_MODEH_MODE15_PUSHPULL;
-#endif
-
-  /* Enable debug clock AUXHFRCO */
-  CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
-
-  /* Wait until clock is ready */
-  while (!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY));
-
-  /* Enable trace in core debug */
-  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-  ITM->LAR  = 0xC5ACCE55;
-  ITM->TER  = 0x0;
-  ITM->TCR  = 0x0;
-  TPI->SPPR = 2;
-  TPI->ACPR = 0xf;
-  ITM->TPR  = 0x0;
-  DWT->CTRL = 0x400003FE;
-  ITM->TCR  = 0x0001000D;
-  TPI->FFCR = 0x00000100;
-  ITM->TER  = 0x1;
-}
+#include "blink.h"
+#include "debug.h"
 
 int main(void)
 {
@@ -73,21 +30,18 @@ int main(void)
   // this call.
   sl_system_init();
 
-  // Initialize the application. For example, create periodic timer(s) or
-  // task(s) if the kernel is present.
-  app_init();
+  blink_init();
 
-  setupSWOForPrint();
+  debug_init();
 
   while (1) {
     // Do not remove this call: Silicon Labs components process action routine
     // must be called from the super loop.
     sl_system_process_action();
 
-    // Application process.
-    app_process_action();
+    blink_process_action();
 
-    ITM_SendChar('f');
+    debug_send_string("foo\n");
 
 #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
     // Let the CPU go to sleep if the system allows it.

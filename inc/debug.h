4f3ad37cf3198344e0026576fdf849930d86b3a8
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2022-09-28 16:00:14 +0200

    Add basic debug functionality

---
+++ inc/debug.h
@@ -0,0 +1,15 @@
+/*
+ * debug.h
+ *
+ *  Created on: 28. sep. 2022
+ *      Author: amatho
+ */
+
+#ifndef INC_DEBUG_H_
+#define INC_DEBUG_H_
+
+void debug_init(void);
+
+void debug_send_string(char *);
+
+#endif /* INC_DEBUG_H_ */

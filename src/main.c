8fb7523c6790f353095c73cdb3fba0256af51eca
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2022-09-28 15:31:27 +0200

    Restructure project

--- src/main.c
+++ src/main.c
@@ -20,11 +20,51 @@
 #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
 #include "sl_power_manager.h"
 #endif
-#if defined(SL_CATALOG_KERNEL_PRESENT)
-#include "sl_system_kernel.h"
-#else // SL_CATALOG_KERNEL_PRESENT
 #include "sl_system_process_action.h"
-#endif // SL_CATALOG_KERNEL_PRESENT
+#include "efm32gg990f1024.h"
+
+void setupSWOForPrint(void)
+{
+  /* Enable GPIO clock. */
+  CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
+
+  /* Enable Serial wire output pin */
+  GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
+
+#if defined(_EFM32_GIANT_FAMILY) || defined(_EFM32_LEOPARD_FAMILY) || defined(_EFM32_WONDER_FAMILY)
+  /* Set location 0 */
+  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
+
+  /* Enable output on pin - GPIO Port F, Pin 2 */
+  GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
+  GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
+#else
+  /* Set location 1 */
+  GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) |GPIO_ROUTE_SWLOCATION_LOC1;
+  /* Enable output on pin */
+  GPIO->P[2].MODEH &= ~(_GPIO_P_MODEH_MODE15_MASK);
+  GPIO->P[2].MODEH |= GPIO_P_MODEH_MODE15_PUSHPULL;
+#endif
+
+  /* Enable debug clock AUXHFRCO */
+  CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
+
+  /* Wait until clock is ready */
+  while (!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY));
+
+  /* Enable trace in core debug */
+  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+  ITM->LAR  = 0xC5ACCE55;
+  ITM->TER  = 0x0;
+  ITM->TCR  = 0x0;
+  TPI->SPPR = 2;
+  TPI->ACPR = 0xf;
+  ITM->TPR  = 0x0;
+  DWT->CTRL = 0x400003FE;
+  ITM->TCR  = 0x0001000D;
+  TPI->FFCR = 0x00000100;
+  ITM->TER  = 0x1;
+}
 
 int main(void)
 {
@@ -37,10 +77,8 @@ int main(void)
   // task(s) if the kernel is present.
   app_init();
 
-#if defined(SL_CATALOG_KERNEL_PRESENT)
-  // Start the kernel. Task(s) created in app_init() will start running.
-  sl_system_kernel_start();
-#else // SL_CATALOG_KERNEL_PRESENT
+  setupSWOForPrint();
+
   while (1) {
     // Do not remove this call: Silicon Labs components process action routine
     // must be called from the super loop.
@@ -49,10 +87,11 @@ int main(void)
     // Application process.
     app_process_action();
 
+    ITM_SendChar('f');
+
 #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
     // Let the CPU go to sleep if the system allows it.
     sl_power_manager_sleep();
 #endif
   }
-#endif // SL_CATALOG_KERNEL_PRESENT
 }
